066c7c40616e827f63adf8cd0778d7682f260c77
abujaforhadi <abujaforhadi@gmail.com>
2025-02-20 12:40:56 +0600

    database added

--- index.js
+++ index.js
@@ -3,9 +3,41 @@ const cors = require('cors')
 require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = process.env.url;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // // Send a ping to confirm a successful connection
+
+    const tasksCollection = client.db("TaskManagement").collection("tasks");
+    
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
     res.send("Task Management server is running...");
